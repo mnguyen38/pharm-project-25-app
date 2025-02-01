f3bd692e7d5df3d46134cf57e2f0919ba6e4b9ad
Minh Nguyen <nguyen.minh8@northeastern.edu>
2025-02-01 18:32:55 -0500

    docstring for cleanIngredients

---
+++ src/IngredientsParsing/cleanIngredients.tsx
@@ -0,0 +1,10 @@
+/**
+ * Cleans and parses a string of ingredients
+ *
+ * @param {string} ingredients - string containing a list of ingredients
+ *
+ */
+export function cleanIngredients(ingredients: string): // add return type 
+{
+
+}
